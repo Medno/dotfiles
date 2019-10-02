95fc5725ab6b032ffa137e43f8b01e828738e221
Philippe Chadenier <phil.chadenier@gmail.com>
2019-10-02 19:08:50 +0200

    Edits

--- init_env.sh
+++ init_env.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+
 ########################################################################
 ##################	Config
 ########################################################################
 
 function	neovim()
 {
-	echo "[+] Installing Neovim and plugins...\n"
+	echo "[+] Installing Neovim and plugins..."
 
 	ln -sfv $PWD/vim/vimrc $PWD/neovim/nvim.vim
 	$pc_get install neovim
+	$pc_get install node
+
 	mkdir -p ~/.config/nvim
+
+	ln -sfv $PWD/neovim/init.vim ~/.config/nvim/init.vim 
+	ln -sfv $PWD/neovim/nvim.vim ~/.config/nvim/nvim.vim 
+	ln -sfv $PWD/neovim/setup_plugins.vim ~/.config/nvim/plugin.vim 
 
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -29,8 +36,8 @@ function	zshrc()
 
 function	install()
 {
-	zshrc();
-	neovim();
+	zshrc
+	neovim
 }
 
 case "$(uname)" in
@@ -38,4 +45,4 @@ case "$(uname)" in
 	"Linux") pc_get="sudo apt-get" ;;
 	*) usage ;;
 esac
-install;
+install
--- neovim/init.vim
+++ neovim/init.vim
@@ -1,2 +1,2 @@
-source $HOME/.config/nvim/setup_plugin.vim
+source $HOME/.config/nvim/plugin.vim
 source $HOME/.config/nvim/nvim.vim
