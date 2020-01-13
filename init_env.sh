b6c70fdfb47be352c586e73730c73cef27b83686
Philippe Chadenier <phil.chadenier@gmail.com>
2020-01-13 09:29:24 +0100

    Check return code of installation of package

--- init_env.sh
+++ init_env.sh
@@ -12,19 +12,32 @@ function	onedark()
 		https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/lightline/colorscheme/onedark.vim
 }
 
+function	install_package()
+{
+	package=$1
+	$pc_check_installed $package >/dev/null
+	if [ $? -ne 0 ]; then
+		$pc_get install $package
+		if [ $? -ne 0 ]; then
+			echo "Error: Unable to install ${bold}$package${normal}"
+			exit 1
+		fi
+	fi
+	echo "${bold}$package${normal} installed"
+}
+
 function	neovim()
 {
 	echo "[+] Installing ${bold}Neovim${normal} and plugins..."
 
-	ln -sfv $PWD/vim/vimrc $PWD/neovim/nvim.vim
 	ln -sfv $PWD/vim/vimrc $HOME/.vimrc 
-	$pc_check_installed neovim >/dev/null || $pc_get install neovim
-	$pc_check_installed node >/dev/null || $pc_get install node
+	install_package neovim
+	install_package node
 
 	mkdir -p ~/.config/nvim
 
 	ln -sfv $PWD/neovim/init.vim ~/.config/nvim/init.vim 
-	ln -sfv $PWD/neovim/nvim.vim ~/.config/nvim/nvim.vim 
+	ln -sfv $PWD/vim/vimrc ~/.config/nvim/nvim.vim 
 	ln -sfv $PWD/neovim/setup_plugins.vim ~/.config/nvim/plugin.vim 
 
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
@@ -58,6 +71,15 @@ function	install()
 	git_config
 }
 
+function	correct_folder()
+{
+	if [ $0 = "./$(basename $0)" ]; then
+		return
+	fi
+	echo "Invalid location, you must be in setup git repository"
+	exit
+}
+
 case "$(uname)" in
 	"Darwin") pc_get="brew"
 		pc_check_installed="brew list";;
@@ -65,4 +87,5 @@ case "$(uname)" in
 		pc_check_installed="dpkg -s";;
 	*) usage ;;
 esac
+correct_folder
 install
