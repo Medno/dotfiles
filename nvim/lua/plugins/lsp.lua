8c6bd7c60643203ab21674c5d09e30350feee29a
Philippe Chadenier <phil.chadenier@gmail.com>
2024-08-05 10:01:19 +0200

    Add goto definition keymaps

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -27,15 +27,15 @@ return {
 		branch = "v2.x",
 		dependencies = {
 			-- LSP Support
-			{ "neovim/nvim-lspconfig" }, -- Required
-			{ "williamboman/mason.nvim" }, -- Optional
+			{ "neovim/nvim-lspconfig" },    -- Required
+			{ "williamboman/mason.nvim" },  -- Optional
 			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
 			-- Autocompletion
 			{ "hrsh7th/nvim-cmp" }, -- Required
 			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
 			{ "L3MON4D3/LuaSnip" }, -- Required
-			{ "j-hui/fidget.nvim", opts = {} },
+			{ "j-hui/fidget.nvim",                opts = {} },
 		},
 		config = function()
 			local lsp = require("lsp-zero").preset({})
@@ -47,6 +47,24 @@ return {
 			end)
 
 			require("lspconfig").pyright.setup({
+				on_attach = function(client, bufnr)
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "Go to Declaration" })
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to Definition" })
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover" })
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation,
+						{ buffer = bufnr, desc = "Go to Implementation" })
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "Signature Help" })
+					-- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename Symbol" })
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr, desc = "Symbol References" })
+					-- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Action" })
+					vim.keymap.set("n", "[d", vim.diagnostic.goto_prev,
+						{ buffer = bufnr, desc = "Go to Next Diagnostic" })
+					vim.keymap.set("n", "gl", vim.diagnostic.open_float,
+						{ buffer = bufnr, desc = "Open Diagnostic Float" })
+					vim.keymap.set("n", "]d", vim.diagnostic.goto_next,
+						{ buffer = bufnr, desc = "Go to Previous Diagnostic" })
+					-- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { buffer = bufnr, desc = "Diagnostic to local list" })
+				end,
 				settings = {
 					pyright = {
 						reportMissingImports = true,
