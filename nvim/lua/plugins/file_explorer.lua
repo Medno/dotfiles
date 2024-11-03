05e59c7dcb2e1c5cfe08888dd481fa016cad55fa
Medno <phil.chadenier@gmail.com>
2024-11-03 23:18:38 +0100

    Add additional config for pymple

--- nvim/lua/plugins/file_explorer.lua
+++ nvim/lua/plugins/file_explorer.lua
@@ -256,7 +256,15 @@ return {
 		"alexpasmantier/pymple.nvim",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			-- optional (nicer ui)
+			"stevearc/dressing.nvim",
+			"nvim-tree/nvim-web-devicons",
 		},
+		build = ":PympleBuild",
+		config = function()
+			require("pymple").setup()
+		end,
 		opts = {
 			update_imports = {
 				filetypes = { "python", "markdown", "config" },
