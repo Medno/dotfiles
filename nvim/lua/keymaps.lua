51ab368d8a46488941ad9f90064d5bc4c10e962b
Medno <phil.chadenier@gmail.com>
2024-11-09 14:12:46 +0100

    Add bufdelete plugin

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -44,6 +44,8 @@ vim.keymap.set("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer"
 vim.keymap.set("n", "[B", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer prev" })
 vim.keymap.set("n", "]B", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer next" })
 
+vim.keymap.set("n", "<leader>bd", "<cmd>Bdelete<cr>", { desc = "Close current buffer" })
+
 -- Trouble
 vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
 vim.keymap.set("n", "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
