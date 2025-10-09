8fcca7bb1d58e2d2f7a4a0a12716363813e3d95a
Philippe Chadenier <phil.chadenier@gmail.com>
2025-10-09 22:30:50 +0200

    add typescript and lua formating, edit padding

--- nvim/lua/plugins/python.lua
+++ nvim/lua/plugins/python.lua
@@ -2,16 +2,17 @@ return {
   "linux-cultist/venv-selector.nvim",
   dependencies = {
     "neovim/nvim-lspconfig",
-    "mfussenegger/nvim-dap",
-    "mfussenegger/nvim-dap-python", --optional
     { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
   },
-  lazy = false,
-  branch = "regexp", -- This is the regexp branch, use this for the new version
+  ft = "python",
   config = function()
     require("venv-selector").setup()
   end,
   keys = {
     { ",v", "<cmd>VenvSelect<cr>" },
+  },
+  opts = {     -- this can be an empty lua table - just showing below for clarity.
+    search = {}, -- if you add your own searches, they go here.
+    options = {} -- if you add plugin options, they go here.
   },
 }
