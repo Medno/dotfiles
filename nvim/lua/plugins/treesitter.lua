081e4de7d42ffb84e61afe65992090bd4108c726
Philippe Chadenier <phil.chadenier@gmail.com>
2025-11-27 17:22:35 +0100

    Specify branch in tree sitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    lazy = false,
     build = ":TSUpdate",
     event = { "BufEnter", "VeryLazy" },
     config = function()
