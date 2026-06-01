ea3516be91ed362bec053468cf4a9320bd4eb281
Philippe Chadenier <phil.chadenier@gmail.com>
2026-06-01 12:05:31 +0200

    comment deprecated treesitter in 0.12

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -3,8 +3,8 @@ return {
     "MeanderingProgrammer/markdown.nvim",
     main = "render-markdown",
     opts = {},
-    name = "render-markdown",                                                       -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+    name = "render-markdown",                    -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { "echasnovski/mini.icons" }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   },
   -- Install markdown preview, use npx if available.
